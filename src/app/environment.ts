e6f9ab5f23e73d93330e3093127567c7d5168630
SIGMA <82468352+3-sigma-moon@users.noreply.github.com>
2024-05-12 13:37:27 +0000

    adding environment file

---
+++ src/app/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  RESOURCES_SERVER_URL: 'http://localhost:3000',
+  COUNTRIES_URL: 'http://localhost:3000/countries',
+  STATES_URL: 'http://localhost:3000/states',
+  STATES_BY_CTR_CODE_URL: 'http://localhost:3000/states?countryCode'
+}
