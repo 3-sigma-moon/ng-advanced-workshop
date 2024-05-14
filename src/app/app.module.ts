04b93ceb44270b166978ebedcda1d87f80971c8e
SIGMA <82468352+3-sigma-moon@users.noreply.github.com>
2024-05-14 22:25:51 +0000

    exercices 6 completed. HostListener(event) HostBinding(property) observables , selectors,generics!!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,13 +26,14 @@ import { StateButtonComponent } from './solution/solution7/state-button.componen
 import { Exercise7Component } from './exercise7/exercise7.component';
 import { CountryDescriptionPipe } from './exercise3/country-description.pipe';
 import { CountryStateComponent } from './exercise5/country-state/country-state.component';
+import { TreeStateDirective } from './exercise6/tree-state.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     Exercise1Component, Solution1Component, Exercise2Component, Solution2Component, Exercise3Component, Solution3Component, HighlightPipe,
     Exercise4Component,CountryStateComponent, Solution4Component, MainPageComponent, Exercise5Component, Solution5Component, AutofilterDropdownComponent,
-    Solution6Component, StateButtonDirective, Exercise6Component, Solution7Component, StateButtonComponent, Exercise7Component, CountryDescriptionPipe, CountryStateComponent
+    Solution6Component, StateButtonDirective, Exercise6Component, Solution7Component, StateButtonComponent, Exercise7Component, CountryDescriptionPipe, CountryStateComponent, TreeStateDirective
   ],
   imports: [
     BrowserModule,
--- src/app/exercise6/exercise6.component.ts
+++ src/app/exercise6/exercise6.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import {timer} from 'rxjs';
+import {Observable, switchMap, tap, timer} from 'rxjs';
+import {CountryService} from "../exercise4/country.service";
+import {State} from "../exercise4/types";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-exercise6',
@@ -8,8 +11,7 @@ import {timer} from 'rxjs';
 })
 export class Exercise6Component  {
 
-  action$ = timer(2000);
-
+  action$ = timer(3000);
 
 }
 
